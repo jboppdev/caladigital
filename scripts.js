e47d53b2d48a5b6820f5c5f9a20a1943ab3a4498
jbopp <eskardado@gmail.com>
2024-06-22 20:14:53 +0200

    plssss dom pslssss

--- scripts.js
+++ scripts.js
@@ -1,50 +1,60 @@
-const itemsPerPage = 5;
-let currentPage = 1;
-
 document.addEventListener('DOMContentLoaded', () => {
-    const listItems = document.querySelectorAll('#article-list li');
-    const totalPages = Math.ceil(listItems.length / itemsPerPage);
+  const itemsPerPage = 5;
+  let currentPage = 1;
+  
+  const listItems = document.querySelectorAll('#article-list li');
+  const totalPages = Math.ceil(listItems.length / itemsPerPage);
 
-    function showPage(page) {
-        listItems.forEach((li, index) => {
-            if (index >= (page - 1) * itemsPerPage && index < page * itemsPerPage) {
-                li.style.display = 'block';
-            } else {
-                li.style.display = 'none';
-            }
-        });
-    }
+  function showPage(page) {
+      listItems.forEach((li, index) => {
+          if (index >= (page - 1) * itemsPerPage && index < page * itemsPerPage) {
+              li.style.display = 'block';
+          } else {
+              li.style.display = 'none';
+          }
+      });
+  }
 
-    function updateButtons() {
-        document.querySelector('button.prev').disabled = currentPage === 1;
-        document.querySelector('button.next').disabled = currentPage === totalPages;
-    }
+  function updateButtons() {
+      const prevButton = document.querySelector('button.prev');
+      const nextButton = document.querySelector('button.next');
+      
+      if (prevButton && nextButton) {
+          prevButton.disabled = currentPage === 1;
+          nextButton.disabled = currentPage === totalPages;
+      }
+  }
 
-    document.querySelector('button.prev').addEventListener('click', () => {
-        if (currentPage > 1) {
-            currentPage--;
-            showPage(currentPage);
-            updateButtons();
-        }
-    });
+  const prevButton = document.querySelector('button.prev');
+  const nextButton = document.querySelector('button.next');
+  
+  if (prevButton && nextButton) {
+      prevButton.addEventListener('click', () => {
+          if (currentPage > 1) {
+              currentPage--;
+              showPage(currentPage);
+              updateButtons();
+          }
+      });
 
-    document.querySelector('button.next').addEventListener('click', () => {
-        if (currentPage < totalPages) {
-            currentPage++;
-            showPage(currentPage);
-            updateButtons();
-        }
-    });
+      nextButton.addEventListener('click', () => {
+          if (currentPage < totalPages) {
+              currentPage++;
+              showPage(currentPage);
+              updateButtons();
+          }
+      });
+  }
 
-    showPage(currentPage);
-    updateButtons();
+  showPage(currentPage);
+  updateButtons();
 
-    // Add separators
-    listItems.forEach(li => {
-        const separator = document.createElement('img');
-        separator.src = './separador.png';
-        separator.alt = 'Separador';
-        separator.classList.add('separator');
-        li.appendChild(separator);
-    });
+  // Add separators
+  listItems.forEach(li => {
+      const separator = document.createElement('img');
+      separator.src = './separador.png';
+      separator.alt = 'Separador';
+      separator.classList.add('separator');
+      li.appendChild(separator);
+  });
 });
